b541c60aad649b34ce071bdf7e7f106bbc70f0b4
Henry Yeung <blackcityhenry@gmail.com>
2024-08-25 20:39:37 +0800

    REINIT: projects created by vite

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
-import Vue from 'vue'
-import './plugins/axios'
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+createApp(App).mount('#app')
